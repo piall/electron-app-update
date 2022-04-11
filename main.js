4191c85e09e498f74f2f549ec27c27268440e343
Md. Pial Ahamed <mdpialahamed@gmail.com>
2022-04-11 13:03:30 +0600

    Create auto-updating app

---
+++ main.js
@@ -0,0 +1,38 @@
+const electorn = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+const { app, BrowserWindow, ipcMain } = electorn;
+
+let mainWindow;
+
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadFile('index.html');
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+mainWindow.once('ready-to-show', () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+function closeNotification() {
+  notification.classList.add('hidden');
+}
+function restartApp() {
+  ipcRenderer.send('restart_app');
+}
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
